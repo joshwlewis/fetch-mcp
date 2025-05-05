568294b2f25150c99019c0b0b82e2aac1165c42f
Josh W Lewis <josh.w.lewis@gmail.com>
2025-05-05 16:12:34 -0500

    Don't fetch name

--- src/index.ts
+++ src/index.ts
@@ -3,135 +3,135 @@
 import { Server } from "@modelcontextprotocol/sdk/server/index.js";
 import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
 import {
-  CallToolRequestSchema,
-  ListToolsRequestSchema,
+    CallToolRequestSchema,
+    ListToolsRequestSchema,
 } from "@modelcontextprotocol/sdk/types.js";
 import { RequestPayloadSchema } from "./types.js";
 import { Fetcher } from "./Fetcher.js";
 
 const server = new Server(
-  {
-    name: "zcaceres/fetch",
-    version: "0.1.0",
-  },
-  {
-    capabilities: {
-      resources: {},
-      tools: {},
+    {
+        name: "zcaceres/fetch",
+        version: "0.1.0",
     },
-  },
+    {
+        capabilities: {
+            resources: {},
+            tools: {},
+        },
+    },
 );
 
 server.setRequestHandler(ListToolsRequestSchema, async () => {
-  return {
-    tools: [
-      {
-        name: "fetch_html",
-        description: "Fetch a website and return the content as HTML",
-        inputSchema: {
-          type: "object",
-          properties: {
-            url: {
-              type: "string",
-              description: "URL of the website to fetch",
+    return {
+        tools: [
+            {
+                name: "fetch_html",
+                description: "Fetch a website and return the content as HTML",
+                inputSchema: {
+                    type: "object",
+                    properties: {
+                        url: {
+                            type: "string",
+                            description: "URL of the website to fetch",
+                        },
+                        headers: {
+                            type: "object",
+                            description: "Optional headers to include in the request",
+                        },
+                    },
+                    required: ["url"],
+                },
             },
-            headers: {
-              type: "object",
-              description: "Optional headers to include in the request",
+            {
+                name: "fetch_markdown",
+                description: "Fetch a website and return the content as Markdown",
+                inputSchema: {
+                    type: "object",
+                    properties: {
+                        url: {
+                            type: "string",
+                            description: "URL of the website to fetch",
+                        },
+                        headers: {
+                            type: "object",
+                            description: "Optional headers to include in the request",
+                        },
+                    },
+                    required: ["url"],
+                },
             },
-          },
-          required: ["url"],
-        },
-      },
-      {
-        name: "fetch_markdown",
-        description: "Fetch a website and return the content as Markdown",
-        inputSchema: {
-          type: "object",
-          properties: {
-            url: {
-              type: "string",
-              description: "URL of the website to fetch",
+            {
+                name: "fetch_txt",
+                description:
+                    "Fetch a website, return the content as plain text (no HTML)",
+                inputSchema: {
+                    type: "object",
+                    properties: {
+                        url: {
+                            type: "string",
+                            description: "URL of the website to fetch",
+                        },
+                        headers: {
+                            type: "object",
+                            description: "Optional headers to include in the request",
+                        },
+                    },
+                    required: ["url"],
+                },
             },
-            headers: {
-              type: "object",
-              description: "Optional headers to include in the request",
+            {
+                name: "fetch_json",
+                description: "Fetch a JSON file from a URL",
+                inputSchema: {
+                    type: "object",
+                    properties: {
+                        url: {
+                            type: "string",
+                            description: "URL of the JSON to fetch",
+                        },
+                        headers: {
+                            type: "object",
+                            description: "Optional headers to include in the request",
+                        },
+                    },
+                    required: ["url"],
+                },
             },
-          },
-          required: ["url"],
-        },
-      },
-      {
-        name: "fetch_txt",
-        description:
-          "Fetch a website, return the content as plain text (no HTML)",
-        inputSchema: {
-          type: "object",
-          properties: {
-            url: {
-              type: "string",
-              description: "URL of the website to fetch",
-            },
-            headers: {
-              type: "object",
-              description: "Optional headers to include in the request",
-            },
-          },
-          required: ["url"],
-        },
-      },
-      {
-        name: "fetch_json",
-        description: "Fetch a JSON file from a URL",
-        inputSchema: {
-          type: "object",
-          properties: {
-            url: {
-              type: "string",
-              description: "URL of the JSON to fetch",
-            },
-            headers: {
-              type: "object",
-              description: "Optional headers to include in the request",
-            },
-          },
-          required: ["url"],
-        },
-      },
-    ],
-  };
+        ],
+    };
 });
 
 server.setRequestHandler(CallToolRequestSchema, async (request) => {
-  const { name, arguments: args } = request.params;
+    const { arguments: args } = request.params;
 
-  const validatedArgs = RequestPayloadSchema.parse(args);
+    const validatedArgs = RequestPayloadSchema.parse(args);
 
-  if (request.params.name === "fetch_html") {
-    const fetchResult = await Fetcher.html(validatedArgs);
-    return fetchResult;
-  }
-  if (request.params.name === "fetch_json") {
-    const fetchResult = await Fetcher.json(validatedArgs);
-    return fetchResult;
-  }
-  if (request.params.name === "fetch_txt") {
-    const fetchResult = await Fetcher.txt(validatedArgs);
-    return fetchResult;
-  }
-  if (request.params.name === "fetch_markdown") {
-    const fetchResult = await Fetcher.markdown(validatedArgs);
-    return fetchResult;
-  }
-  throw new Error("Tool not found");
+    if (request.params.name === "fetch_html") {
+        const fetchResult = await Fetcher.html(validatedArgs);
+        return fetchResult;
+    }
+    if (request.params.name === "fetch_json") {
+        const fetchResult = await Fetcher.json(validatedArgs);
+        return fetchResult;
+    }
+    if (request.params.name === "fetch_txt") {
+        const fetchResult = await Fetcher.txt(validatedArgs);
+        return fetchResult;
+    }
+    if (request.params.name === "fetch_markdown") {
+        const fetchResult = await Fetcher.markdown(validatedArgs);
+        return fetchResult;
+    }
+    throw new Error("Tool not found");
 });
 
 async function main() {
-  const transport = new StdioServerTransport();
-  await server.connect(transport);
+    const transport = new StdioServerTransport();
+    await server.connect(transport);
 }
 
 main().catch((error) => {
-  console.error("Fatal error in main():", error);
-  process.exit(1);
+    console.error("Fatal error in main():", error);
+    process.exit(1);
 });
